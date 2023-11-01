4bb88eec68e5d36decd6fc2b0c4c37c5ee790b5c
oscar <oriquelme@rfranco.com>
2023-11-01 17:38:14 +0100

    finish hill encrypt/decrypt

--- hill/hill.py
+++ hill/hill.py
@@ -23,6 +23,7 @@ def checkPositive(value):
 
     return int(value)
 
+
 def mcd(a, b):
     while b > 0:
         a, b = b, a % b
@@ -42,7 +43,7 @@ def hill():
 
     args = parser.parse_args()
 
-    outputFile = open(args.o) if args.o else sys.stdout
+    outputFile = open(args.o, "w") if args.o else sys.stdout
     keys = matrix.fromFile(args.k)
     m = int(args.m)
     n = int(args.n)
@@ -55,30 +56,63 @@ def hill():
         parser.error("You can't specify both -D and -C")
 
     if keys is None:
-        raise error(f"Failed to read matrix from the path provided by -k argument ${args.k}")
+        raise error(
+            f"Failed to read matrix from the path provided by -k argument {args.k}"
+        )
 
     if len(keys[0]) != n:
-        raise error(f"Length mismatch! Matrix defined in ${args.k} should be of length ${n}x${n}")
+        raise error(
+            f"Length mismatch! Matrix defined in {args.k} should be of length {n}x{n}"
+        )
 
     if mcd(matrix.determinant(keys), m) != 1:
-        raise error(f"The matrix defined in ${args.k} is not inyective and decrypting is not possible")
+        raise error(
+            f"The matrix defined in {args.k} is not inyective and decrypting is not possible"
+        )
 
     userInput = readInput(args)
     if args.C == True:
         print("Cifrando")
-        encrypt(userInput, outputFile, keys, m)
+        encrypt(userInput, outputFile, keys, m, n)
 
     elif args.D == True:
         print("Descifrando")
-        decrypt(userInput, outputFile, keys, m)
+        decrypt(userInput, outputFile, keys, m, n)
 
 
-def encrypt(userInput, outputFile, keysMatrix, m):
-    return 0
+def encrypt(userInput, outputFile, keysMatrix, m, n):
+    asciiPadding = ord("A")
+    # add padding so that the array is dividible in chunks of n size without leftovers
+    while len(userInput) % n != 0:
+        userInput += "@"
+
+    inputMatrix = matrix.fromString(userInput)
+    # split the array into n chunks
+    for i in range(0, len(inputMatrix), n):
+        chunk = inputMatrix[i : i + n]
+
+        cypheredChunk = keysMatrix.dot(chunk) % m
+        for cypheredCharacter in cypheredChunk:
+            outputFile.write(chr(cypheredCharacter + asciiPadding))
 
 
-def decrypt(userInput, outputFile, keysMatrix, m):
-    return 0
+def decrypt(userInput, outputFile, keysMatrix, m, n):
+    asciiPadding = ord("A")
+    # add padding so that the array is dividible in chunks of n size without leftovers
+    while len(userInput) % n != 0:
+        userInput += "@"
+
+    inputMatrix = matrix.fromString(userInput)
+    inversedMatrix = matrix.inverse(keysMatrix, m)
+
+    # split the array into n chunks
+    for i in range(0, len(inputMatrix), n):
+        chunk = inputMatrix[i : i + n]
+        
+        decryptedChunk = inversedMatrix.dot(chunk) % m
+        for decryptedCharacter in decryptedChunk:
+            outputFile.write(chr(round(decryptedCharacter) + asciiPadding))
+
 
 if __name__ == "__main__":
     sys.exit(hill())
--- hill/matrix.py
+++ hill/matrix.py
@@ -1,7 +1,9 @@
 import numpy as np
+
 """ 
 Simple wrapper for working with matrix for our use case. 
 """
+
 
 def fromFile(path):
     try:
@@ -12,20 +14,76 @@ def fromFile(path):
         print(f"Error: Could not read the file at {path}")
         return None
 
+
 def determinant(matrix):
-    try:
-        det = np.linalg.det(matrix)
-        return round(det)
+    det = np.linalg.det(matrix)
+    return int(det)
 
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
 
-def inverse(matrix):
-    try:
-        inverse = np.linalg.inv(matrix)
-        return inverse
+def extendedEuclides(a, m):
+    if m == 0:
+        return (1, 0, a)
 
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
+    x1, y1, gcd = extendedEuclides(m, a % m)
+    x = y1
+    y = x1 - (a // m) * y1
+
+    return x, y, gcd
+
+
+def cofactor(matrix):
+    """
+    Calculate cofactor matrix of A
+    """
+    sel_rows = np.ones(matrix.shape[0], dtype=bool)
+    sel_columns = np.ones(matrix.shape[1], dtype=bool)
+    cofactor = np.zeros_like(matrix)
+    sgn_row = 1
+    for row in range(matrix.shape[0]):
+        # Unselect current row
+        sel_rows[row] = False
+        sgn_col = 1
+        for col in range(matrix.shape[1]):
+            # Unselect current column
+            sel_columns[col] = False
+
+            subMatrix = matrix[sel_rows][:, sel_columns]
+            cofactor[row, col] = sgn_row * sgn_col * (round(np.linalg.det(subMatrix)))
+
+            # Reselect current column
+            sel_columns[col] = True
+            sgn_col = -sgn_col
+
+        sel_rows[row] = True
+        # Reselect current row
+        sgn_row = -sgn_row
+
+    return cofactor
+
+
+def adjugate(matrix):
+    return cofactor(matrix).T
+
+
+def inverse(matrix, m):
+    # Find x in module m so that it satisfies that x * det(k) == 1 mod(m).
+    # X will be equal to 1/det(k) in mod(m). We do this using the extended euclidean algorithm
+    matrixDeterminant = determinant(matrix) % m
+    # inversedDeterminant, _, _ = extendedEuclides(matrixDeterminat, m)
+    inversedDeterminant = -1
+    for i in range(m):
+        x = i * matrixDeterminant
+        if x % m == 1:
+            inversedDeterminant = i
+            break
+
+    adjugateMatrix = adjugate(matrix)
+    # adjugateMatrix.put(18, 19)
+    return (inversedDeterminant * adjugateMatrix) % m
+
+
+def fromString(inputString):
+    asciiPadding = ord("A")
+    asciiList = [ord(char) - asciiPadding for char in inputString.upper()]
+
+    return np.array(asciiList, dtype=int)
